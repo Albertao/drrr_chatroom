aee4d7ad583d363ca8ec3430841312354371e892
albert <807134187@qq.com>
2016-04-30 19:10:33 +0800

    熟悉swoole原生websocket写法

--- index.php
+++ index.php
@@ -1,20 +1,33 @@
 <?php
-//开启debug模式
-define('DEBUG', 'on');
-define('WEBPATH', __DIR__);
+$server = new swoole_websocket_server('0.0.0.0', 9777);
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+$users = $redis->sMembers('users');
 
-require WEBPATH.'/vendor/autoload.php';
+//开启连接时进行房间广播并将用户存入redis集合中
+$server->on('open', function(swoole_websocket_server $server, $request) use ($redis){
+    $redis->sAdd('users', $request->fd);
+    echo "zxczxc";
 
-//swoole framework init
-Swoole\Loader::vendor_init();
+});
 
-Swoole\Loader::addNameSpace('drrr', __DIR__.'server_side');
+//接收信息时对房间进行广播
+$server->on('message', function(swoole_websocket_server $server, $frame) use ($users){
+    $data['msg'] = json_decode($frame->data)['msg'];
+    $data['status'] = 1;
+    foreach ($users as $user) {
+        if ($user != $frame->fd) {
+            $server->push($user, $data);
+        }
+    }
+});
 
-$config = require WEBPATH.'/config.php';
+$server->on('close', function(swoole_websocket_server $server, $fd) use($users){
+    foreach ($users as $user) {
+        if($user != $fd){
+            $server->push($user, "{$fd} exit");
+        }
+    }
+});
 
-$drrr_server = new drrr\server;
-$drrr_server->loadSetting($config);
-
-$serv = new Swoole\Network\Server($config['server']['host'], $config['server']['port']);
-$serv->setProtocol($drrr_server);
-$serv->run($config['drrr']);
+$server->start();
